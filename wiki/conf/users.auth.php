c619841550f570b7f58ab98a14ec2491b484666a
andijunior <45861631+andijunior@users.noreply.github.com>
2018-12-27 01:54:56 +0100

    Upload dokuwiki

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+andyjunior:$1$Xh39v2Bj$jEJatqWKMkhPn31TL70nj.:Andy:andyjunior@t-online.de:admin,user
